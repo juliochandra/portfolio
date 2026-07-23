174650c75d45397d6e712e5caede4f023e88e872
juliochandra <juliochandra@gmail.com>
2026-07-23 11:35:38 +0700

    feat(admin): add password change page

---
+++ src/app/admin/password/page.tsx
@@ -0,0 +1,10 @@
+import { PasswordForm } from "@/app/admin/password/_components/PasswordForm";
+
+export default function PasswordPage() {
+	return (
+		<section>
+			<h1 className="font-bold text-3xl tracking-tight">Password</h1>
+			<PasswordForm />
+		</section>
+	);
+}
